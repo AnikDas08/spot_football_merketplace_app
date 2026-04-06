4794df638065039abb5f7c35fa60a433fd497025
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-04-06 14:26:07 +0600

    solve

--- lib/utils/constants/app_icons.dart
+++ lib/utils/constants/app_icons.dart
@@ -6,6 +6,7 @@ class AppIcons {
   static const String burger = '$baseurl/burger.svg';
   //Add by arif
   static const String homeActive = '$baseurl/home_active.svg';
+
   static const String engTvInActive = '$baseurl/eng_tv_in_active.svg';
   static const String fixturesInActive = '$baseurl/fixtures_in_active.svg';
   static const String statsInactive = '$baseurl/stats_inactive.svg';
@@ -13,4 +14,10 @@ class AppIcons {
   static const String transfersInActive= '$baseurl/transfers_in_astive.svg';
 
 
+
+
+
+  // added by ajijul
+  static const String notification = '$baseurl/notification_inactive.svg';
+  static const String menu = '$baseurl/menu.svg';
 }
