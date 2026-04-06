f41fd12a17db612f2d0a8dc4904282716f2a96e9
anik das <anikd5656@gmail.com>
2026-04-06 09:17:32 +0600

    add setup

---
+++ lib/config/route/app_routes.dart
@@ -0,0 +1,42 @@
+import 'package:get/get.dart';
+
+class AppRoutes {
+  /*static const String test = '/test_screen.dart';
+  static const String splash = '/';
+  static const String onboarding = '/onboarding_screen.dart';
+  static const String signUp = '/sign_up_screen.dart';
+  static const String verifyUser = '/verify_user.dart';
+  static const String signIn = '/sign_in_screen.dart';
+  static const String forgotPassword = '/forgot_password.dart';
+  static const String verifyEmail = '/verify_screen.dart';
+  static const String createPassword = '/create_password.dart';
+  static const String changePassword = '/change_password_screen.dart';
+  static const String notifications = '/notifications_screen.dart';
+  static const String chat = '/chat_screen.dart';
+  static const String message = '/message_screen.dart';
+  static const String profile = '/profile_screen.dart';
+  static const String editProfile = '/edit_profile.dart';
+  static const String privacyPolicy = '/privacy_policy_screen.dart';
+  static const String termsOfServices = '/terms_of_services_screen.dart';
+  static const String setting = '/setting_screen.dart';*/
+
+  static List<GetPage<String>> routes = [
+    /*GetPage(name: splash, page: () => const SplashScreen()),
+    GetPage(name: onboarding, page: () => const OnboardingScreen()),
+    GetPage(name: signUp, page: () => SignUpScreen()),
+    GetPage(name: verifyUser, page: () => const VerifyUser()),
+    GetPage(name: signIn, page: () => SignInScreen()),
+    GetPage(name: forgotPassword, page: () => ForgotPasswordScreen()),
+    GetPage(name: verifyEmail, page: () => VerifyScreen()),
+    GetPage(name: createPassword, page: () => CreatePassword()),
+    GetPage(name: changePassword, page: () => ChangePasswordScreen()),
+    GetPage(name: notifications, page: () => const NotificationScreen()),
+    GetPage(name: chat, page: () => const ChatListScreen()),
+    GetPage(name: message, page: () => const MessageScreen()),
+    GetPage(name: profile, page: () => const ProfileScreen()),
+    GetPage(name: editProfile, page: () => EditProfile()),
+    GetPage(name: privacyPolicy, page: () => const PrivacyPolicyScreen()),
+    GetPage(name: termsOfServices, page: () => const TermsOfServicesScreen()),
+    GetPage(name: setting, page: () => const SettingScreen()),*/
+  ];
+}
