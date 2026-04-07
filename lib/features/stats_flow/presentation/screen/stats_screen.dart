f6c8a60585cdd90300bd522543c3a8927958ea3c
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-04-07 13:59:12 +0600

    tranasfer form screen done

---
+++ lib/features/stats_flow/presentation/screen/stats_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/component/common_appbar/common_appbar.dart';
+import 'package:untitled/component/text/common_text.dart';
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: CommonAppbar(  ),
+
+
+    );
+  }
+}
