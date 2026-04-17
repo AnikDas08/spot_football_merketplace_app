6444f557a572135ac6f8e93fe4dfaa56560c8fc9
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-04-17 15:31:22 +0600

    all fixed

--- lib/features/auth/sign up/presentation/screen/successful_create_account.dart
+++ lib/features/auth/sign up/presentation/screen/successful_create_account.dart
@@ -4,29 +4,12 @@ import 'package:get/get.dart';
 import 'package:untitled/component/image/common_image.dart';
 import '../../../../../../../config/route/app_routes.dart';
 import '../../../../../../../utils/constants/app_colors.dart';
-import '../../../../../../../utils/constants/app_string.dart';
-import '../../../../../../../utils/extensions/extension.dart';
-import '../../../../../../../utils/helpers/validation.dart';
 import '../../../../../component/button/common_button.dart';
 import '../../../../../component/text/common_text.dart';
-import '../../../../../component/text_field/common_text_field.dart';
 import '../../../sign in/presentation/widgets/signup_appbar.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'package:untitled/component/image/common_image.dart';
 import 'package:untitled/services/storage/storage_keys.dart';
 import 'package:untitled/services/storage/storage_services.dart';
-import '../../../../../../../config/route/app_routes.dart';
-import '../../../../../../../utils/constants/app_colors.dart';
-import '../../../../../../../utils/constants/app_string.dart';
-import '../../../../../../../utils/extensions/extension.dart';
-import '../../../../../../../utils/helpers/validation.dart';
-import '../../../../../component/button/common_button.dart';
-import '../../../../../component/text/common_text.dart';
-import '../../../../../component/text_field/common_text_field.dart';
-import '../../../sign in/presentation/widgets/signup_appbar.dart';
 
 class SuccessfulCreateAccount extends StatelessWidget {
   SuccessfulCreateAccount({super.key});
