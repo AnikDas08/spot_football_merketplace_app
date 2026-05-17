1376409d3e3c13d8b4b42d13b2ec69e423538ff8
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-05-17 16:21:21 +0600

    remove unnecessary whitespace in club_profile_controller.dart for improved code readability

--- lib/features/home/presentation/controllers/club_profile_controller.dart
+++ lib/features/home/presentation/controllers/club_profile_controller.dart
@@ -7,7 +7,6 @@ class ClubProfileController extends ChangeNotifier {
   ClubProfileController() {
     getUpcomingFixtures();
   }
-
   void getUpcomingFixtures() {
     List<Map<String, dynamic>> dummyData = [
       {"date": "OCT 12", "homeTeam": "Titans SC", "awayTeam": "Vortex FC", "time": "20:00 PM"},
@@ -15,7 +14,6 @@ class ClubProfileController extends ChangeNotifier {
       {"date": "OCT 20", "homeTeam": "Lions FC", "awayTeam": "Titans SC", "time": "21:00 PM"},
       {"date": "OCT 24", "homeTeam": "Vortex FC", "awayTeam": "Phoenix UTDS", "time": "19:30 PM"},
     ];
-
     upcomingFixturesList = dummyData.map((e) => UpcomingFixtureModel.fromJson(e)).toList();
 
     notifyListeners();
