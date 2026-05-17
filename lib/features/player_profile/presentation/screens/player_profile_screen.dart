fb83985e831a5592b570a4911ba311319be58db0
Md Arif Bhuiyan <your_id+username@users.noreply.github.com>
2026-05-17 16:55:41 +0600

    remove unused imports in news_details_screen.dart and overview_tab.dart for improved code clarity

--- lib/features/player_profile/presentation/screens/player_profile_screen.dart
+++ lib/features/player_profile/presentation/screens/player_profile_screen.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:untitled/component/common_appbar/common_appbar.dart';
 import 'package:untitled/component/common_appbar/secondary_appbar.dart';
 import 'package:untitled/features/home/presentation/widgets/latest_news.dart';
 import 'package:untitled/features/home/presentation/widgets/latest_videos.dart';
-import 'package:untitled/features/home/presentation/widgets/news_card.dart';
-import 'package:untitled/features/player_profile/presentation/widgets/performance_card.dart';
+
 import 'package:untitled/utils/constants/app_colors.dart';
 import 'package:untitled/utils/constants/app_string.dart';
 
-import '../../../../component/text/common_text.dart';
 import '../controllers/player_profile_controller.dart';
 import '../widgets/eng_record_widget.dart';
 import '../widgets/personal_details_widget.dart';
